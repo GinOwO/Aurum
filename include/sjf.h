2ee056fe59b887e70da515c43fb84351984cd261
Ambuj Singh <136119784+s-ambuj@users.noreply.github.com>
2023-08-29 16:30:00 +0530

    Update sjf.h

--- include/sjf.h
+++ include/sjf.h
@@ -1,9 +1,9 @@
 #ifndef _AURUM_SHORTEST_JOB_FIRST_H
 #define _AURUM_SHORTEST_JOB_FIRST_H 1
 
-#include "process.h"    // Path: include/process.h
-#include "queues.h"     // Path: include/queues.h
-#include "basealg.h"    // Path: include/basealg.h
+#include "process.h"         // Path: include/process.h
+#include "queues.h"          // Path: include/queues.h
+#include "basealg.h"         // Path: include/basealg.h
 #include "ctexceptions.h"    // Path: include/ctexceptions.h
 
 class ShortestJobFirst : public BaseAlgorithm{
