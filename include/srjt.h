21dff02de754b3b77e0eb0923fd66b76c1f5b894
Ambuj Singh <136119784+s-ambuj@users.noreply.github.com>
2023-08-29 17:52:34 +0530

    Update  srjt.h

--- include/srjt.h
+++ include/srjt.h
@@ -1,14 +1,14 @@
-#ifndef _AURUM_SHORTEST_JOB_FIRST_H
-#define _AURUM_SHORTEST_JOB_FIRST_H 1
+#ifndef _AURUM_SHORTEST_REMAINING_JOB_FIRST_H
+#define _AURUM_SHORTEST_REMAINING_JOB_FIRST_H 1
 
 #include "process.h"         // Path: include/process.h
 #include "queues.h"          // Path: include/queues.h
 #include "basealg.h"         // Path: include/basealg.h
 #include "ctexceptions.h"    // Path: include/ctexceptions.h
 
-class ShortestJobFirst : public BaseAlgorithm{
+class ShortestRemainingJobFirst : public BaseAlgorithm{
 public:
-    ShortestJobFirst(Queue*,Queue*,Queue*,Queue*,Queue*,int,int);
+    ShortestRemainingJobFirst(Queue*,Queue*,Queue*,Queue*,Queue*,int,int);
     void run();
     static bool comparator(Process *p1, Process *p2);
 };
